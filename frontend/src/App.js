3fdf51909c6d24107967f9662be30a7a742369dc
HebaAzf <heba.azeef@gmail.com>
2023-12-30 17:05:44 -0500

    FirstCommit

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+//used 'rafce' to create this arrow function component
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1>The BIPOC Hub Showcase</h1>
+  )
+}
+
+export default App
